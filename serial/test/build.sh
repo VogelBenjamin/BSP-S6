d0996eaea25a9978d5a9a412c23b32adf91a928c
VogelBenjamin <benjamin.vogel.001@student.uni.lu>
2025-03-13 20:55:19 +0100

    decomposed testing for different matrices and wrote bash functions

--- serial/test/build.sh
+++
@@ -1,8 +0,0 @@
-gcc LAS_test.c ../src/LAS_serial.c ../src/cg_serial.c -o LAS.out -lm
-mv LAS.out bin
-./bin/LAS.out
-
-
-gcc cg_test.c ../src/LAS_serial.c ../src/cg_serial.c ../../matrices/matrix_load.c ../../matrices/mmio.c -o cg.out -lm
-mv cg.out bin
-./bin/cg.out
