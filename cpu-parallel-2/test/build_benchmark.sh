190c11374fe85d42a0b1299b27b9bde913c9ade3
VogelBenjamin <benjamin.vogel.001@student.uni.lu>
2025-03-17 08:12:25 +0100

    modified benchmark batch file

--- cpu-parallel-2/test/build_benchmark.sh
+++ cpu-parallel-2/test/build_benchmark.sh
@@ -6,19 +6,19 @@
 module purge
 module load toolchain/foss/2020b
 
-gcc bcsstk06_test.c ../src/LAS_serial.c ../src/cg_serial.c ../../matrices/matrix_load.c ../../matrices/mmio.c -o bcsstk06.out -lm
+gcc bcsstk06_test.c ../src/LAS_cpu_parallel.c ../src/cg_cpu_parallel.c ../../matrices/matrix_load.c ../../matrices/mmio.c -o bcsstk06.out -lm -O3 -march=native -fopenmp -ffast-math -funroll-loops 
 mv bcsstk06.out bin
 ./bin/bcsstk06.out > output/bcsstk06_benchmark.txt
 
-gcc bcsstk16_test.c ../src/LAS_serial.c ../src/cg_serial.c ../../matrices/matrix_load.c ../../matrices/mmio.c -o bcsstk16.out -lm
+gcc bcsstk16_test.c ../src/LAS_cpu_parallel.c ../src/cg_cpu_parallel.c ../../matrices/matrix_load.c ../../matrices/mmio.c -o bcsstk16.out -lm -O3 -march=native -fopenmp -ffast-math -funroll-loops 
 mv bcsstk16.out bin
 ./bin/bcsstk16.out > output/bcsstk16_benchmark.txt
 
-gcc bcsstk17_test.c ../src/LAS_serial.c ../src/cg_serial.c ../../matrices/matrix_load.c ../../matrices/mmio.c -o bcsstk17.out -lm
+gcc bcsstk17_test.c ../src/LAS_cpu_parallel.c ../src/cg_cpu_parallel.c ../../matrices/matrix_load.c ../../matrices/mmio.c -o bcsstk17.out -lm -O3 -march=native -fopenmp -ffast-math -funroll-loops 
 mv bcsstk17.out bin
 ./bin/bcsstk17.out > output/bcsstk17_benchmark.txt
 
-gcc bcsstk13_test.c ../src/LAS_serial.c ../src/cg_serial.c ../../matrices/matrix_load.c ../../matrices/mmio.c -o bcsstk13.out -lm
+gcc bcsstk13_test.c ../src/LAS_cpu_parallel.c ../src/cg_cpu_parallel.c ../../matrices/matrix_load.c ../../matrices/mmio.c -o bcsstk13.out -lm -O3 -march=native -fopenmp -ffast-math -funroll-loops 
 mv bcsstk13.out bin
 ./bin/bcsstk13.out > output/bcsstk13_benchmark.txt
 
