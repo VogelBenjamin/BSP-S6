5e1c9c6586024d4d4d8011215b1c8db090abf970
VogelBenjamin <benjamin.vogel.001@student.uni.lu>
2025-03-30 15:42:15 +0200

    updated the cluster benchmark to test for various levels of threads and cpus

--- cpu-parallel-2/test/bcsstk06_test.c
+++ cpu-parallel-2/test/bcsstk06_test.c
@@ -2,13 +2,21 @@
 #include<stdlib.h>
 #include<assert.h>
 #include<time.h>
+#include<omp.h>
 #include"../src/cg_cpu_parallel.h"
 #include"../../matrices/matrix_load.h"
 
 #define epsilon 1E-9
 
-int main()
+int main(int arg, char* args[])
 {
+    if (arg !=2 )
+    {
+        return -1;
+    }
+    int num_threads = atoi(args[1]);
+    omp_set_num_threads(num_threads);
+    printf("input %d, num_threads %d\n",atoi(args[1]),omp_get_max_threads());
     // BCSSTK06
     double* cg_solution;
     double* FFGE_matrix;
--- cpu-parallel-2/test/build_cluster_benchmark.sh
+++ cpu-parallel-2/test/build_cluster_benchmark.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -c 56
+#SBATCH --socket-per-node=2
 #SBATCH --time=22:00:00
 
 module purge
@@ -8,18 +9,38 @@ module load toolchain/foss/2020b
 
 gcc bcsstk06_test.c ../src/LAS_cpu_parallel.c ../src/cg_cpu_parallel.c ../../matrices/matrix_load.c ../../matrices/mmio.c -o bcsstk06.out -lm -O3 -march=native -fopenmp -ffast-math -funroll-loops 
 mv bcsstk06.out bin
-./bin/bcsstk06.out > output/bcsstk06_benchmark.txt 8
 
 gcc bcsstk16_test.c ../src/LAS_cpu_parallel.c ../src/cg_cpu_parallel.c ../../matrices/matrix_load.c ../../matrices/mmio.c -o bcsstk16.out -lm -O3 -march=native -fopenmp -ffast-math -funroll-loops 
 mv bcsstk16.out bin
-./bin/bcsstk16.out > output/bcsstk16_benchmark.txt 8
 
 gcc bcsstk17_test.c ../src/LAS_cpu_parallel.c ../src/cg_cpu_parallel.c ../../matrices/matrix_load.c ../../matrices/mmio.c -o bcsstk17.out -lm -O3 -march=native -fopenmp -ffast-math -funroll-loops 
 mv bcsstk17.out bin
-./bin/bcsstk17.out > output/bcsstk17_benchmark.txt 8
 
 gcc bcsstk13_test.c ../src/LAS_cpu_parallel.c ../src/cg_cpu_parallel.c ../../matrices/matrix_load.c ../../matrices/mmio.c -o bcsstk13.out -lm -O3 -march=native -fopenmp -ffast-math -funroll-loops 
 mv bcsstk13.out bin
-./bin/bcsstk13.out > output/bcsstk13_benchmark.txt 8
 
-#{ time ./pH proton.txt 28 10 0 0 0 3 2 4 7 4 2 ;} 2>> output/timeCompare.txt
+#!/bin/bash
+
+# Define the directory to store output files
+OUTPUT_DIR="output"
+
+# Create the directory if it doesn't exist
+mkdir -p "$OUTPUT_DIR"
+
+# Define the list of integers
+NUMBERS=(8 56 28 14 8 4 2 9 13 27 45 62)
+
+# Define the list of executables
+EXECUTABLES=("bin/bcsstk06.out" "bin/bcsstk13.out" "bin/bcsstk16.out", bin/bcsstk17.out)
+
+# Loop over the integers
+for num in "${NUMBERS[@]}"; do
+    for exe in "${EXECUTABLES[@]}"; do
+        exe_name=$(basename "$exe")
+        # Execute the binary file with the integer as an argument
+        # Redirect the output to a file named using the executable name and integer
+        ./$exe "$num" > "$OUTPUT_DIR/${exe_name}_$num.txt"
+    done
+done
+
+echo "Execution complete. Output files are in '$OUTPUT_DIR'"
