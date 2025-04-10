e4c8627fdef51315c8996b855183e1bd0ff0dc6b
VogelBenjamin <benjamin.vogel.001@student.uni.lu>
2025-04-10 17:14:46 +0200

    compile c and cu files and link them together using nvcc

--- gpu-parallel/correct/build_test.sh
+++ gpu-parallel/correct/build_test.sh
@@ -16,7 +16,4 @@ nvcc -c ../src/LAS_gpu_parallel.cu -o LAS_gpu_parallel.o
 nvcc -c ../src/cg_gpu_parallel.cu -o cg_gpu_parallel.o
 
 nvcc -o main bcsstk06.o matrix_load.o mmio.o LAS_gpu_parallel.o cg_gpu_parallel.o -lm
-mv b06.out bin
-
-./bin/b06.out
 
--- gpu-parallel/src/cg_gpu_parallel.h
+++ gpu-parallel/src/cg_gpu_parallel.h
@@ -1,1 +1,15 @@
+#ifndef CG_GPU_PARALLEL_H
+#define CG_GPU_PARALLEL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 float* cg(unsigned int size, float* A, float* b, float* init_g, float epsilon, int debug);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
